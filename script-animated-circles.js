7f2dce225440dac9eff530daf0d67588732affda
Harjeet Singh <hskainth@itdc.co.in>
2022-10-08 05:00:45 +0530

    portfolio Website Landing:2.3

--- script-animated-circles.js
+++ script-animated-circles.js
@@ -11,36 +11,49 @@ const mouseCircleFn=(x,y) => {
 
 // Animated Circles
 const circles = document.querySelectorAll(".circle");
-const mainImg = document.querySelectorAll(".main-circle img");
+const mainImg = document.querySelector(".main-circle img");
 
 let mX=0;
 let mY=0;
 
 const animatedCircles=(e,x,y)=>{
+
    
-    if(x>mX)
+    if(x<mX)
     {
         // console.log("moved to right");
         circles.forEach((circle)=>{
             circle.style.left=`100px`;
         });
+        mainImg.style.left=`100px`;
     }
-    /* else if(x<mX)
+    else if(x>mX)
     {
-        console.log("moved to left");
-    } */
+        // console.log("moved to (right)");
+        
+        circles.forEach((circle)=>{
+            circle.style.left=`-100px`;
+        });
+        mainImg.style.left=`-100px`;
+    }
 
-    if(y>mY)
+    if(y<mY)
     {
         // console.log("moved to upward");
         circles.forEach((circle)=>{
+            circle.style.top=`100px`;
+        });
+        mainImg.style.top=`100px`;
+    }
+    else if(y>mY)
+    {
+        // console.log("moved to (upward)");
+
+        circles.forEach((circle)=>{
             circle.style.top=`-100px`;
         });
+        mainImg.style.top=`-100px`;
     }
-    /* else if(y<mY)
-    {
-        console.log("moved to downward");
-    } */
     mX=e.clientX;
     mY=e.clientY;
 };
